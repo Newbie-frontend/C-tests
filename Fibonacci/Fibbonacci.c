6ea00a519d6f0e6c6a3f0d388e7295db4f2e0154
HR <hos.raz1993@gmail.com>
2021-12-13 01:44:45 +0300

    Fibbonatcci

---
+++ Fibonacci/Fibbonacci.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+long getFibbTerm (int t) {
+    if ( t == 1 || t == 2 )
+        return 1;
+    return getFibbTerm(t-1) + getFibbTerm(t-2);
+}
+
+int main() {
+    printf("Enter the range : ");
+    int n;
+    scanf("%d", &n);
+    int t;
+    for (t = 1 ; t <= n; t++)
+        printf("%ld\n", getFibbTerm(t));
+    return 0;
+}
