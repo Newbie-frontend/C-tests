c6abe393b9e0d41d069c440a510e90ed4ae9c22f
HR <hos.raz1993@gmail.com>
2022-01-03 16:09:15 +0300

    10

---
+++ Pointer/10.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+void bileklikCoz(int num, char *pc, int *ph, int *ps){
+    int a;
+    a = num /10000;
+    if (a == 1)
+    {
+        *pc = 'E';
+    }else if (a == 2)
+    {
+        *pc = 'K';
+    }
+    
+    
+    *ph = (num % 10000)/100;
+    *ps =(num %100);
+    
+}
+
+int main (){
+    int num;
+    char cinsiyet;
+    int hafta;
+    int siraNumarasi;
+    printf("Sayiyi giriniz : ");
+    scanf("%d", &num);
+    bileklikCoz(num, &cinsiyet, &hafta, &siraNumarasi);
+    printf("Cinsiyet : %c\n", cinsiyet);
+    printf("Hafta : %d\n", hafta);
+    printf("Sira numarasi : %d\n", siraNumarasi);
+
+    return 0;
+}
--- Pointer/test/mallocCallocRealloc.c
+++ Pointer/test/mallocCallocRealloc.c
@@ -1,4 +1,4 @@
-
+dd
 #include <stdio.h>
 #include <stdlib.h>
 
