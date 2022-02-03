4c0be4b31c4ed2d791d5ba1a938c1536e1905fa5
HR <hos.raz1993@gmail.com>
2022-02-04 01:55:55 +0300

    5

--- String/5.c
+++ String/5.c
@@ -1,21 +1,44 @@
 #include <stdio.h>
 #include <ctype.h>
 
-
 int main()
 {
     char str[100];
 
     printf("Dizgiyi giriniz : ");
     scanf("%[^\n]", str);
+    printf("---------------------------------\n");
+    int count = 0;
     for (int i = 0; str[i] != '\0'; i++)
     {
-        if (isupper(str[i]))
-            str[i] = tolower(str[i]);
-        else if (islower(str[i]))
-            str[i] = toupper(str[i]);
+        if (str[i] == 'a' || str[i] == 'ı' || str[i] == 'o' || str[i] == 'u')
+        {
+            for (int j = i + 1; str[j] != '\0'; j++)
+                if (str[j] == 'e' || str[j] == 'i' || str[j] == 'ö' || str[j] == 'ü')
+                {
+                    printf("Buyuk Sesli Uyumuna Uymaz");
+                    count = 1;
+                    break;
+                }
+            if (count == 1)
+                break;
+        }
+        else if (str[i] == 'e' || str[i] == 'i' || str[i] == 'ö' || str[i] == 'ü')
+        {
+            for (int j = i + 1; str[j] != '\0'; j++)
+                if (str[j] == 'a' || str[j] == 'ı' || str[j] == 'o' || str[j] == 'u')
+                {
+                    printf("Buyuk Sesli Uyumuna Uymaz");
+                    count = 1;
+                    break;
+                }
+            if (count == 1)
+                break;
+        }
     }
+    if (count == 0)
+        printf("Buyuk Sesli Uyumuna Uyar");
+    
 
-    printf("Sonuc dizgi : %s \n", str);
     return 0;
 }
