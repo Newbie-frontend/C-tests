33883d84e59c81bb20123e77ec3543b6b97235ee
HR <hos.raz1993@gmail.com>
2022-02-06 01:43:06 +0300

    33

---
+++ String/33.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <math.h>
+
+void spacemask(char *str);
+
+int main()
+{
+    char sample[] = "This is a test";
+    spacemask(sample);
+    printf("%s", sample);
+
+    return 0;
+}
+
+void spacemask(char *str)
+{
+    for (int i = 0; *(str + i) != '\0'; i++)
+        if (*(str + i) == ' ')
+            *(str + i) = '_'; 
+}
