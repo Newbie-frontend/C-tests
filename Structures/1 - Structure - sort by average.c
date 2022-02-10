38fc0ed8063a9d79138cbd797e444f3ff002ff91
HR <hos.raz1993@gmail.com>
2022-02-10 17:20:06 +0300

    1 2

---
+++ Structures/1 - Structure - sort by average.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+typedef struct ogrenci
+{
+    char ad[20];
+    char soyAd[20];
+    double ort;
+} ogrenci;
+
+int main(int argc, char const *argv[])
+{
+    ogrenci a[10];
+    for (size_t i = 0; i < 10; i++)
+    {
+        printf("%d. ogrencinin adi, soyadi, ortalamasini giriniz : ", i + 1);
+        scanf("%s %s %lf", &a[i].ad, &a[i].soyAd, &a[i].ort);
+    }
+    printf("--------------LISTE---------------\n");
+    for (size_t i = 0; i < 9; i++)
+    {
+        ogrenci temp;
+        for (size_t j = 0; j < 9; j++)
+            if (a[j].ort < a[j + 1].ort)
+            {
+                temp = a[j + 1];
+                a[j + 1] = a[j];
+                a[j] = temp;
+            }
+    }
+    for (size_t i = 0; i < 10; i++)
+        printf("%s %s %0.2lf \n", a[i].ad, a[i].soyAd, a[i].ort);
+
+    return 0;
+}
