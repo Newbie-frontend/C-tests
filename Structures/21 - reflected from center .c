a424962111bc5c0f279144ca86051875529187d2
HR <hos.raz1993@gmail.com>
2022-02-11 23:31:24 +0300

    21

--- Structures/21 - reflected from center .c
+++ Structures/21 - reflected from center .c
@@ -8,27 +8,24 @@ typedef struct Dortgen
     int sagAltY;
 } Dortgen;
 
-void simetrikBul(Dortgen *a)
+void simetrikBul(Dortgen a, Dortgen *temp)
 {
-    Dortgen temp;
-    temp = *a;
-    temp.solUstX = a->sagAltX * (-1);
-    temp.solUstY = a->sagAltY * (-1);
-    temp.sagAltX = a->solUstX * (-1);
-    temp.sagAltY = a->solUstY * (-1);
-    *a = temp;
+    temp->solUstX = a.sagAltX * (-1);
+    temp->solUstY = a.sagAltY * (-1);
+    temp->sagAltX = a.solUstX * (-1);
+    temp->sagAltY = a.solUstY * (-1);
 }
 
 int main(int argc, char const *argv[])
 {
-    Dortgen a;
+    Dortgen a, b;
     printf("Orijinalin bilgilerini giriniz (sol ust, sag alt): ");
     scanf("%d %d %d %d", &a.solUstX, &a.solUstY, &a.sagAltX, &a.sagAltY);
 
-    simetrikBul(&a);
+    simetrikBul(a, &b);
     printf("--------------------------------\n");
     printf("Simetrik dikdortgen bilgileri : \n");
-    printf("sol ust x, y = %d, %d ", a.solUstX, a.solUstY);
-    printf("sag alt x, y = %d, %d ", a.sagAltX, a.sagAltY);
+    printf("sol ust x, y = %d, %d \n", b.solUstX, b.solUstY);
+    printf("sag alt x, y = %d, %d \n", b.sagAltX, b.sagAltY);
     return 0;
 }
